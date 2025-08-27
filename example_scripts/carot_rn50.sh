0fd54e3ddec8ef39173428257db7a897bcbd7496
HesamAsad <hesam.as.sa.as@gmail.com>
2025-08-27 11:34:16 +1000

    toy experiment updated

--- example_scripts/carot_rn50.sh
+++ example_scripts/carot_rn50.sh
@@ -4,25 +4,26 @@ cd /data/projects/punim1316/CaRot
 echo "working directory: $PWD"
 export PYTHONPATH="$PYTHONPATH:$PWD"
 
-lr=6e-6
-wd=0.01
-bs=224
+lr=9e-6
+wd=0.02
+bs=512
 ts=0.0
-method=carot
+method=carot_rn50
 fp16=1
+seed=0
 
-for sd in 1.1
+for sd in 0.01
 do
-  for alpha_fd in 2000.0
+  for alpha_fd in 200.0
   do
 
-    CUDA_VISIBLE_DEVICES='0,1' python src/main.py \
+    python src/main.py \
       --train-dataset ImageNet --epochs 10 --lr ${lr} --wd ${wd} --batch-size ${bs} \
-      --model ViT-L/14 --eval-datasets ImageNet,ImageNetV2,ImageNetR,ImageNetA,ImageNetSketch,ObjectNet \
-      --template openai_imagenet_template --save ./checkpoints/ \
+      --model RN50 --eval-datasets ImageNet,ImageNetV2,ImageNetR,ImageNetA,ImageNetSketch,ObjectNet \
+      --template openai_imagenet_template --save ./checkpoints/ --seed ${seed} \
       --data-location ./datasets/data/ --ft_data ./datasets/csv/imagenet.csv \
-      --csv-img-key filepath --csv-caption-key title --exp_name ImageNet/${method} --cross_fnorm 0.05 \
-      --distil_coef ${sd} --l_orth_wv 0 --max_grad_norm 0 --grad_norm_multiplier 0 --warmup_length 150 \
+      --csv-img-key filepath --csv-caption-key title --exp_name ImageNet/${method} --cross_fnorm 0.01 \
+      --distil_coef ${sd} --l_orth_wv 0 --max_grad_norm 0 --grad_norm_multiplier 0 --warmup_length 500 \
       --wb_project clip_finetune --method ${method} --use_fp16 ${fp16} --run 2 --ema_up_freq 0 \
       --alpha_fd ${alpha_fd} --alpha_cross_kd 1.0 --alpha_icl 1.0 --alpha_crd 1.0 --workers 32
 
